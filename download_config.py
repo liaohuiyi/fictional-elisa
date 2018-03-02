d15b2dda128126b637d9f437c567e87c0dc5fb57
liaohuiyi <yolanda_liao@hotmail.com>
2018-03-02 17:13:30 +0800

    remove all files...

--- download_config.py
+++
@@ -1,5 +0,0 @@
-DOWNLOAD_STUDY_LIST = [
-{'hospital_id' : 'fastDFS1', 'study_id' : 'CT0078451'},
-{'hospital_id' : 'fastDFS1', 'study_id' : 'CT0163795'}
-]
-
--- remove_config.py
+++
@@ -1,4 +0,0 @@
-REMOVE_STUDY_LIST = [
-{'hospital_id' : 'hosplhy', 'study_id' : 'CT0093730'},
-{'hospital_id' : 'hosplhy', 'study_id' : 'P00001836'} 
-]
